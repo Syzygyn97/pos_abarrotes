e6d62f6ecbe6ce1711d15397421b664818d89f0e
Cesar Daniel Ramos Pedroza <albarnengland@hotmail.com>
2026-06-13 18:32:07 -0600

    Primer commit del Pryecto

---
+++ pos_abarrotes/src/main/java/com/abarrotes/pos/vista/POS_Abarrotes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.abarrotes.pos.vista;
+
+/**
+ *
+ * @author danie
+ */
+public class POS_Abarrotes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

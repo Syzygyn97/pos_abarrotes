4f99e6632fc9dc85d623d0460b4e4d0b9669e61b
Kikilinklok <kikilinklok@gmail.com>
2026-06-23 21:41:52 -0600

    Add files via upload

---
+++ pos_abarrotes/target/pos_abarrotes/src/main/java/com/abarrotes/pos/vista/POS_Abarrotes.java
@@ -0,0 +1,15 @@
+package com.abarrotes.pos.vista;
+
+// Si necesitas imports, van aquí abajo del package, por ejemplo:
+// 
+
+public class POS_Abarrotes {
+
+    public static void main(String[] args) {
+        // Tu lógica de inicio aquí
+        frmLogin login = new frmLogin();
+        login.setVisible(true);
+        login.setLocationRelativeTo(null);
+    }
+    
+}
